151373efd4b0a57090637c588570d45ab450a6ca
DrPenknife <rkozik.utp@gmail.com>
2022-02-18 15:48:35 +0100

    changes

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -5,6 +5,8 @@ import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 nltk.download('punkt')
+
+
 from nltk.tokenize import word_tokenize
 from transformers import AutoTokenizer, DistilBertConfig, TFDistilBertModel, DistilBertTokenizerFast
 
@@ -83,6 +85,7 @@ keylen = 100
 
 
 class DataLoader():
+
     def store_results(self,wyniki,dataset,method,db='swarog_wyniki_v1.sqlite'):
         curt = time.time()
         conn = sqlite3.connect(db)
@@ -106,192 +109,49 @@ class DataLoader():
         cursor.close()
     
 
-    def setpaths(self,dsname='fakenewskdd2020', readcb=None):
+    def setpaths(self,dsname='fakenewskdd2020', readcb=None, environment="colab"):
         self.readcb = readcb
         self.datasetname = dsname
         self.pickleversion = -1
         print("Using", self.datasetname, 'dataset')
-        try:
-            import google.colab
+
+        if environment = "colab":
             self.IN_COLAB = True
             self.path = f'/content/drive/MyDrive/swarog/datasets/{self.datasetname}/'
             self.modelspath=f"/content/drive/MyDrive/swarog/models/{self.datasetname}/"
             self.modelspath2=f"/content/drive/MyDrive/swarog/models/"
             self.pickleversion = 5
             print('using colab')
-            print(self.path)
-            print(self.modelspath)
-        except:
-            self.IN_FNKDDCOLAB = False
-            import os
-            if 'nt' == os.name:
-                self.pickleversion = 5
-                print("using my windows...")
-                self.path = f'c:/Users/demo/Desktop/sciwork/data/swarog/datasets/{self.datasetname}/'
-                self.modelspath = f'c:/Users/demo/Desktop/sciwork/data/swarog/models/{self.datasetname}/'
-                self.modelspath2 = f'c:/Users/demo/Desktop/sciwork/data/swarog/models/'
-            else:
-                print('using local')
-                self.path = f'/media/rkozik/0C129CFC129CEBC8/data/swarog/datasets/{self.datasetname}/'
-                self.modelspath=f"/media/rkozik/0C129CFC129CEBC8/data/swarog/models/{self.datasetname}/"
-                self.modelspath2=f"/media/rkozik/0C129CFC129CEBC8/data/swarog/models/"
 
+        elif environment = "mypc1":
+            self.pickleversion = 5
+            print("using my windows...")
+            self.path = f'c:/Users/demo/Desktop/sciwork/data/swarog/datasets/{self.datasetname}/'
+            self.modelspath = f'c:/Users/demo/Desktop/sciwork/data/swarog/models/{self.datasetname}/'
+            self.modelspath2 = f'c:/Users/demo/Desktop/sciwork/data/swarog/models/'
+        
+        elif environment = "mypc2":
+            print('using local')
+            self.path = f'/media/rkozik/0C129CFC129CEBC8/data/swarog/datasets/{self.datasetname}/'
+            self.modelspath=f"/media/rkozik/0C129CFC129CEBC8/data/swarog/models/{self.datasetname}/"
+            self.modelspath2=f"/media/rkozik/0C129CFC129CEBC8/data/swarog/models/"
+
+        print(self.path)
+        print(self.modelspath)
 
 
     def set_path(self,x):
         self.path = x
-        
-        
-    def read(self):
-        if self.readcb:
-            return self.readcb(self)
-        
-        #
-        # Default parsers!
-        #
-        if self.datasetname == "grafn":
-            from pandasql import sqldf
-            _fake = pd.read_csv(self.path + "badnews.csv", sep=",").dropna()
-            _true = pd.read_csv(self.path + "goodnews.csv", sep=",").dropna()
-            real = sqldf("select text, 0 as label from _true where length(text) > 200 limit 20000", locals())
-            print("GRAFN LABELS:\n Fake=",_fake.shape[0], " True=", _true.shape[0])
-            return _fake, real
-        
-        if self.datasetname == "isot":
-            _fake = pd.read_csv(self.path + "Fake.csv", sep=",").dropna()
-            _true = pd.read_csv(self.path + "True.csv", sep=",").dropna()
-            print("ISOT LABELS:\n Fake=",_fake.shape[0], " True=", _true.shape[0])
-            return _fake, _true
-  
-        if self.datasetname == "fakenewskdd2020":
-            from pandasql import sqldf
-            print("load",self.path + "train.csv")
-            dsfile = pd.read_csv(self.path + "train.csv",sep="\t")
-            fake = sqldf("select text, label from dsfile where label=1 and length(text) > 10", locals())
-            real = sqldf("select text, label from dsfile where label=0 and length(text) > 10", locals())
-            print("FN-KDD LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
-            return fake, real
-        #
-        # orgins:
-        # https://zenodo.org/record/4282522#.Yga6zt-M6Lk
-        if self.datasetname == "covid19fn":
-            from pandasql import sqldf
-            print("load",self.path + "data.csv")
+
+
+    #
+    #
+    # ##### generate intermediate representations 
+    # 
+    #
+
  
-            # yes, labels are swpped 
-            fake = sqldf("select headlines as text, outcome from dsfile where outcome=0 and length(text) > 10", locals())
-            real = sqldf("select headlines as text, outcome from dsfile where outcome=1 and length(text) > 10", locals())
-            print("Covid19 LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
-            return fake, real
-
-        if self.datasetname == "mmcovid":
-            from pandasql import sqldf
-            print("load",self.path + "news_collection.csv")
-            dsfile = pd.read_csv(self.path + "news_collection.csv",sep="\t")
-            fake = sqldf("select text, label from dsfile where label='fake' and lang='en' and length(text) > 10", locals())
-            real = sqldf("select text, label from dsfile where label='real' and lang='en' and length(text) > 10", locals())
-            print("MM-Covid19 LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
-            return fake, real
-        
-        
-        if self.datasetname == "pubhealth":
-            from pandasql import sqldf
-            print("load",self.path + "train.tsv")
-            dsfile = pd.read_csv(self.path + "train.tsv",sep="\t")
-            fake = sqldf("""select main_text as text, 
-                            label from dsfile where label='false' and length(main_text) > 10""", locals())
-            real = sqldf("""select main_text as text, 
-                            label from dsfile where label='true' and length(main_text) > 10""", locals())
-            print("PUBHealth LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
-            return fake, real
-
-        if self.datasetname == "politifact":
-            from pandasql import sqldf
-            print("load",self.path + "train.csv")
-            dsfile = pd.read_csv(self.path + "train.csv",sep="\t")
-            real = sqldf("""select Statement as text, Rating as
-                            label from dsfile where Rating <= 2 and length(Statement) > 10""", locals())
-            fake = sqldf("""select Statement as text, Rating as
-                            label from dsfile where Rating > 2 and length(Statement) > 10""", locals())
-            print("PolitiFact LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
-            return fake, real
- 
-
-    def sumarise_text(self,text):
-        words = (text).split(" ")
-        sentences = sent_tokenize(text)
-        if len(sentences) <= 5:
-            return text
-        freqTable = dict()
-        for bword in words:
-            word = bword.lower()
-            if word in freqTable:
-                freqTable[word] += 1
-            else:
-                freqTable[word] = 1
-        sentenceValue = dict()
-        sentenceLen = dict()
-        for sentence in sentences:
-            sentence_key = sentence[:keylen]
-            word_count_in_sentence = len((sentence).split(" "))
-            sentenceLen[sentence_key] = word_count_in_sentence
-            for wordValue in freqTable:
-                if wordValue in sentence.lower():
-                    if sentence_key in sentenceValue:
-                        sentenceValue[sentence_key] += freqTable[wordValue]
-                    else:
-                        sentenceValue[sentence_key] = freqTable[wordValue]
-            if sentence_key in sentenceValue:
-                sentenceValue[sentence_key] = sentenceValue[sentence_key] / word_count_in_sentence
-        arr = []
-        for sentence in sentences:
-            sentence_key = sentence[:keylen]
-            if sentence_key in sentenceValue:
-                arr.append((sentenceLen[sentence_key],sentenceValue[sentence_key]))
-        arr = sorted(arr, key=lambda tup: -tup[1])
-        acum = 0
-        i = 0
-        while True:
-            acum = acum + arr[i][0]
-            if (i+1) >= len(arr) or acum + arr[i+1][0] >= MAX_SEQUENCE_LENGTH:
-                break
-            i = i+1
-        threshold =  arr[i][1]
-        sentence_count = 0
-        summary = ''
-        for sentence in sentences:
-            if sentence[:keylen] in sentenceValue and sentenceValue[sentence[:keylen]] > (threshold):
-                summary += " " + sentence
-                sentence_count += 1
-        return summary
-
-    def sumarise_dataset(self,txtN):
-        i = 0
-        print("sumarise_dataset")
-        for it in range(0,len(txtN)):
-            doc = txtN[it]
-            if i%6000 == 0 or (i+1)==len(txtN):
-                print(i+1,"/",len(txtN))
-            i=i+1  
-            if len(doc) < 5*MAX_SEQUENCE_LENGTH:
-                continue
-            words = doc.split(" ")
-            if len(words) > MAX_SEQUENCE_LENGTH:
-                sd = self.sumarise_text(doc)
-                doc_len = len(sd)
-                if doc_len < 10:
-                    pass
-                    # print("wird length of sumarised sentence len=",doc_len,"was", len(doc) )
-                else:
-                    txtN[it] = sd
-        return txtN    
-
     def remove_stop_words(self,fakes):
-        # table = str.maketrans('', '', string.punctuation)
-        # stripped = [w.translate(table) for w in words]
-        # stripped
-        
-        
         stop_words = set(stopwords.words())
         txtY = []
         i = 0
@@ -350,34 +210,9 @@ class DataLoader():
         fake, good = self.read()
         txtY = self.remove_stop_words(fake['text'])
         txtN = self.remove_stop_words(good['text'])
-#         txtY=self.sumarise_dataset(txtY)
-#         txtN=self.sumarise_dataset(txtN)
-        
         self.store_sumarised(txtY, txtN)
         t = self.fit_tokenizer(txtY,txtN)
         self.tokenize_corpus(txtY,txtN,t)
-
-
-    def load_raw_txt(self):
-        fake, good = self.read()    
-        tn = good['text'].tolist()
-        ty = fake['text'].tolist()
-        ylab = [1 for i in range(0,len(ty))]
-        nlab = [0 for i in range(0,len(tn))]
-        return (ty+tn, ylab+nlab)
-    
- 
-    def load_raw_txt_as_pandas(self):
-        txt, lab = self.load_raw_txt()
-        return pd.DataFrame({'text':txt, 'labels':lab})
- 
-
-    def load_compact(self):
-        with open(self.path + 'x_compact.pickle', 'rb') as handle:
-            x = pickle.load(handle)
-        with open(self.path + 'y_compact.pickle', 'rb') as handle:
-            y = pickle.load(handle)
-        return (np.array(x), np.array(y))
 
     def make_doc2vec(self, mode="avg", top_nnum=MAX_SEQUENCE_LENGTH, data=None):
         self.model = KeyedVectors.load_word2vec_format(self.modelspath2 + 
@@ -405,33 +240,105 @@ class DataLoader():
                 pickle.dump(x, handle, protocol=pickle.HIGHEST_PROTOCOL)
             
         return x
+
+    #
+    #
+    # ##### Loaders 
+    # 
+    #
+
+    def read(self):
+        if self.readcb:
+            return self.readcb(self)
         
-    def sumarized_to_csv(self):
-        ty,tn = self.load_sumarised()
+        #
+        # Default parsers!
+        #
+        if self.datasetname == "grafn":
+            from pandasql import sqldf
+            _fake = pd.read_csv(self.path + "badnews.csv", sep=",").dropna()
+            _true = pd.read_csv(self.path + "goodnews.csv", sep=",").dropna()
+            real = sqldf("select text, 0 as label from _true where length(text) > 200 limit 20000", locals())
+            print("GRAFN LABELS:\n Fake=",_fake.shape[0], " True=", _true.shape[0])
+            return _fake, real
+        
+        if self.datasetname == "isot":
+            _fake = pd.read_csv(self.path + "Fake.csv", sep=",").dropna()
+            _true = pd.read_csv(self.path + "True.csv", sep=",").dropna()
+            print("ISOT LABELS:\n Fake=",_fake.shape[0], " True=", _true.shape[0])
+            return _fake, _true
+  
+        if self.datasetname == "fakenewskdd2020":
+            from pandasql import sqldf
+            print("load",self.path + "train.csv")
+            dsfile = pd.read_csv(self.path + "train.csv",sep="\t")
+            fake = sqldf("select text, label from dsfile where label=1 and length(text) > 10", locals())
+            real = sqldf("select text, label from dsfile where label=0 and length(text) > 10", locals())
+            print("FN-KDD LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
+            return fake, real
+
+        if self.datasetname == "covid19fn":
+            from pandasql import sqldf
+            print("load",self.path + "data.csv")
+ 
+            # yes, labels are swpped 
+            fake = sqldf("select headlines as text, outcome from dsfile where outcome=0 and length(text) > 10", locals())
+            real = sqldf("select headlines as text, outcome from dsfile where outcome=1 and length(text) > 10", locals())
+            print("Covid19 LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
+            return fake, real
+
+        if self.datasetname == "mmcovid":
+            from pandasql import sqldf
+            print("load",self.path + "news_collection.csv")
+            dsfile = pd.read_csv(self.path + "news_collection.csv",sep="\t")
+            fake = sqldf("select text, label from dsfile where label='fake' and lang='en' and length(text) > 10", locals())
+            real = sqldf("select text, label from dsfile where label='real' and lang='en' and length(text) > 10", locals())
+            print("MM-Covid19 LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
+            return fake, real
+        
+        
+        if self.datasetname == "pubhealth":
+            from pandasql import sqldf
+            print("load",self.path + "train.tsv")
+            dsfile = pd.read_csv(self.path + "train.tsv",sep="\t")
+            fake = sqldf("""select main_text as text, 
+                            label from dsfile where label='false' and length(main_text) > 10""", locals())
+            real = sqldf("""select main_text as text, 
+                            label from dsfile where label='true' and length(main_text) > 10""", locals())
+            print("PUBHealth LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
+            return fake, real
+
+        if self.datasetname == "politifact":
+            from pandasql import sqldf
+            print("load",self.path + "train.csv")
+            dsfile = pd.read_csv(self.path + "train.csv",sep="\t")
+            real = sqldf("""select Statement as text, Rating as
+                            label from dsfile where Rating <= 2 and length(Statement) > 10""", locals())
+            fake = sqldf("""select Statement as text, Rating as
+                            label from dsfile where Rating > 2 and length(Statement) > 10""", locals())
+            print("PolitiFact LABELS:\n Fake=",fake.shape[0], " True=", real.shape[0])
+            return fake, real
+
+    def load_raw_txt(self):
+        fake, good = self.read()    
+        tn = good['text'].tolist()
+        ty = fake['text'].tolist()
         ylab = [1 for i in range(0,len(ty))]
         nlab = [0 for i in range(0,len(tn))]
-        pdf = pd.DataFrame({'x':ty+tn , 'y':ylab+nlab})
-        pth = self.path + 'sumarized.csv'
-        pdf.to_csv(pth)
-        print('saved to',pth)
-        
-    def sumarized_to_flair_corpus(self):
-        from pandasql import sqldf
-        ty,tn = self.load_sumarised()
-        ylab = ['__label__fake' for i in range(0,len(ty))]
-        nlab = ['__label__real' for i in range(0,len(tn))]
-        pdf = pd.DataFrame({'text':ty+tn , 'label':ylab+nlab})
-        
-        data = sqldf("select label, text  from pdf order by random()", locals())
-        
-        pth = self.path + 'flair_sumarized_'
-        
-        data.iloc[0:int(len(data)*0.8)].to_csv(pth + 'train.csv', sep='\t', index = False, header = False)
-        data.iloc[int(len(data)*0.8):int(len(data)*0.9)].to_csv(pth + 'test.csv', sep='\t', index = False, header = False)
-        data.iloc[int(len(data)*0.9):].to_csv(pth + 'dev.csv', sep='\t', index = False, header = False);
-  
-        print('saved to',pth+'*')
+        return (ty+tn, ylab+nlab)
+ 
+    def load_raw_txt_as_pandas(self):
+        txt, lab = self.load_raw_txt()
+        return pd.DataFrame({'text':txt, 'labels':lab})
 
+    def load_compact(self):
+        with open(self.path + 'x_compact.pickle', 'rb') as handle:
+            x = pickle.load(handle)
+        with open(self.path + 'y_compact.pickle', 'rb') as handle:
+            y = pickle.load(handle)
+        return (np.array(x), np.array(y))
+
+        
     def load_doc2vec(self, mode="avg"):
         filepath = self.path + 'wor2vec_'+mode+'.pickle'
         print("opening",filepath)
